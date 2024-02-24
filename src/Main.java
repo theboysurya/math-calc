66b8936fdf3fa50dd6704f36bc873e39da496995
theboysurya <thesuryasingh9@gmail.com>
2024-02-25 01:22:40 +0530

    checkPrime added

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+//        System.out.println("Welcome to math-calc");
+        System.out.println("Welcome to math-calc\n1. Count Number of Factors\n2. Check for Prime Number\nChoose:");
+        int option = scn.nextInt();
+        System.out.println("Enter your number: ");
         int number = scn.nextInt();
-        int factorCount = countFactor(number);
-        System.out.println(factorCount);
-
-
+        switch (option){
+            case 1:
+                int factorCount = countFactor(number);
+                System.out.println(factorCount);
+                break;
+            case 2:
+                String checkPrime = checkPrime(number);
+                System.out.println(checkPrime);
+                break;
+        }
     }
 
     public static int countFactor(int number){
@@ -26,5 +36,17 @@ public class Main {
             }
         }
         return count;
+    }
+
+    public static String checkPrime(int number){
+        String isPrime = "";
+        int countFactor = countFactor(number);
+        if(countFactor > 2) {
+            isPrime = "NO";
+        } else {
+            isPrime = "YES";
+        }
+
+        return isPrime;
     }
 }

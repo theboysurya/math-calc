b1f72dc49bec972b5c017a16b317daba65ee594d
theboysurya <thesuryasingh9@gmail.com>
2024-02-25 01:03:39 +0530

    countFactor Done

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int number = scn.nextInt();
+        int factorCount = countFactor(number);
+        System.out.println(factorCount);
 
+
+    }
+
+    public static int countFactor(int number){
+        int count = 0;
+        for(int i = 1; i <= Math.sqrt(number); i++){
+            if(number % i == 0) { //Checking for i is factor or not
+                if (i == number / i) {
+                    count += 1;
+//                    System.out.println(i);
+                } else {
+                    count += 2;
+//                    System.out.println(i + ", " + number / i);
+                }
+            }
+        }
+        return count;
     }
 }
